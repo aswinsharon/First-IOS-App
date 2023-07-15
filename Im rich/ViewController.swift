8598b17ef1a7d4edb417e15f4b1174b9807aee8c
Aswin Sharon <aswinsharon@computer.local>
2023-07-15 23:41:22 +0530

    first commit

---
+++ Im rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Im rich
+//
+//  Created by Aswin Sharon on 15/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
